b6e6716b14eaf930101c336b7632358a366d7e1c
mrzor <eli@mrzor.com>
2017-09-13 20:34:49 +0200

    add a couple of naive endpoints

--- src/httpbis/routes/home.clj
+++ src/httpbis/routes/home.clj
@@ -8,13 +8,35 @@
 
 (defn ^:endpoint
       ^{:doc "Returns a new UUID4"
-        :mount ["/uuid" "/uuid/4"]
+        :route "/uuid"
         :verb :GET}
       uuid4 []
   {:status 200
    :httpbis/jsonify true
    :body {:uuid (str (uuid/v4))}})
 
-(defroutes home-routes
-  (GET "/" [] (home))
-  (GET "/uuid" [] (uuid4)))
+(defn ^:endpoint
+      ^{:doc "Returns Origin IP."
+        :route "/ip"
+        :verb :GET}
+      ip [request]
+  {:status 200
+   :httpbis/jsonify true
+   :body {:origin (:remote-addr request)}})
+
+(defn ^:endpoint
+^{:doc "Returns user-agent."
+  :route "/user-agent"
+  :verb :GET}
+user-agent [request]
+  {:status 200
+   :httpbis/jsonify true
+   :body {:user-agent (get-in request [:headers "user-agent"])}})
+
+(def home-routes
+  (routes
+    (GET "/" [] (home))
+    (GET "/uuid" [] (uuid4))
+    (GET "/ip" request (ip request))
+    (GET "/user-agent" request (user-agent request))
+    ))
